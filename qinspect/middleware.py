d995f9e72d1a2940aaafe89229aff8371fa81a5b
Senko Rasic <senko.rasic@dobarkod.hr>
2014-06-13 11:15:27 +0200

    fix logging compatibility with Python 2.6

--- qinspect/middleware.py
+++ qinspect/middleware.py
@@ -9,8 +9,15 @@ from django.conf import settings
 from django.db import connection
 from django.db.backends.util import CursorDebugWrapper
 
+if hasattr(logging.NullHandler):
+    NullHandler = logging.NullHandler
+else:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
 log = logging.getLogger(__name__)
-log.addHandler(logging.NullHandler())
+log.addHandler(NullHandler())
 
 cfg = dict(
     enabled=(settings.DEBUG and
