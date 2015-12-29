8c29790e2e3757354cae7de1434dcaeb12c16167
Senko Rasic <senko.rasic@dobarkod.hr>
2015-12-29 13:54:57 +0100

    advertise support for Django 1.9 and Python 3.5

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,6 +1,15 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url, include
+from django import VERSION
+
+from testapp.views import get_authors_with_books, book
+
+if VERSION >= (1, 9):
+    def patterns(prefix, *args):
+        return args
+else:
+    from django.conf.urls import patterns
 
 urlpatterns = patterns('testapp.views',
-    url(r'^authors/$', 'get_authors_with_books', name='authors'),
-    url(r'^book/$', 'book', name='book'),
+    url(r'^authors/$', get_authors_with_books, name='authors'),
+    url(r'^book/$', book, name='book'),
 )
